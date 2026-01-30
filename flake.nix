2fdbb5fd1fdd47f78d90928fc806c11da7799382
Michael Whitehead <onepunchlinux@gmail.com>
2026-01-30 12:23:05 -0700

    fix yarn

--- flake.nix
+++ flake.nix
@@ -4,18 +4,35 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = (import (inputs.nixpkgs) { inherit system; });
-      in {
+      in
+      {
         devShell = pkgs.mkShell {
-          buildInputs= with pkgs; [
+          buildInputs = with pkgs; [
+            nodejs_22
             yarn
             nodePackages.pnpm
             nodePackages.typescript
             nodePackages.typescript-language-server
           ];
+
+          shellHook = ''
+            export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
+
+            # Put Corepack shims in a writable place (NOT /nix/store)
+            mkdir -p "$HOME/.local/bin"
+            export PATH="$HOME/.local/bin:$PATH"
+
+            corepack enable --install-directory "$HOME/.local/bin"
+
+            echo "Node: $(node -v)"
+            echo "Yarn: $(yarn --version)"
+          '';
         };
       }
     );
